fb2128158d382f8b837624e921396091d8292e06
bastilian <sebastian.graessl@gmail.com>
2009-05-06 21:01:06 +0200

    remove setters

--- lib/fluxapi/account.rb
+++ lib/fluxapi/account.rb
@@ -3,7 +3,6 @@ class Fluxiom::Account < Fluxiom
     self.class.call('/api/account.xml')['account'].each do |k, v|
       self.instance_variable_set("@#{k}", v)
       self.class.send(:define_method, k, proc{self.instance_variable_get("@#{k}")})
-      self.class.send(:define_method, "#{k}=", proc{|v| self.instance_variable_set("@#{k}", v)})
     end
   end
   def assets
--- lib/fluxapi/user.rb
+++ lib/fluxapi/user.rb
@@ -3,7 +3,6 @@ class Fluxiom::User < Fluxiom
     data.each do |k, v|
       self.instance_variable_set("@#{k}", v)
       self.class.send(:define_method, k, proc{self.instance_variable_get("@#{k}")})
-      self.class.send(:define_method, "#{k}=", proc{|v| self.instance_variable_set("@#{k}", v)})
     end
   end 
 end
