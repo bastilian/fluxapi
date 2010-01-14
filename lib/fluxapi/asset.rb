33a9c033d657d0126afad4b1793cbfbb78559634
bastilian <bastilian@validcode.me>
2010-01-14 16:11:21 +0100

    * update method for downloading * add tag-method to tag an asset

--- lib/fluxapi/asset.rb
+++ lib/fluxapi/asset.rb
@@ -3,14 +3,21 @@ class Fluxiom::Asset < Fluxiom
     data.each do |k, v|
       self.instance_variable_set("@#{k}", v)
       self.class.send(:define_method, k, proc{self.instance_variable_get("@#{k}")})
+      self.class.send(:define_method, "#{k}=", proc{|v| self.instance_variable_set("@#{k}", v)})
     end
   end
+  
+  def tag(t)
+    res = self.class.post_call("/api/assets/#{self.id}/tag", :body => {:tags => t.api_name})
+    self.tags = self.tags.to_s + ' ' + t.api_name
+  end
+  
   def download(path_to_file)
-    session = Net::HTTP.new(self.class.base_uri.gsub('https://', ''), 443)
+    session = Net::HTTP.new(@@base_url.gsub('https://', ''), 443)
     session.use_ssl = true
     session.start {|http|
-      req = Net::HTTP::Get.new("/api/assets/download/#{self.id}")
-      req.basic_auth self.class.default_options[:basic_auth][:username], self.class.default_options[:basic_auth][:password]
+      req = Net::HTTP::Get.new("/api/assets/#{self.id}/download")
+      req.basic_auth @@user, @@password
       resp = http.request(req)
       open(File.join(path_to_file, self.filename), 'wb') do |file|
         file.write(resp.body)
