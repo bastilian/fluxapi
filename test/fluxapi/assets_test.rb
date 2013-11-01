2bb7da248868d51af193ceef315686c00ccef74e
Sebastian Gräßl <sebastian@validcode.me>
2013-11-01 23:54:50 +0100

    fix tests

--- test/fluxapi/assets_test.rb
+++ test/fluxapi/assets_test.rb
@@ -8,15 +8,15 @@ class Fluxiom::AssetsTest < Test::Unit::TestCase
   end
 
   def test_assets
-    assert @assets, Fluxiom::Assets
+    assert_equal @assets.class, Fluxiom::Assets
   end
 
   def test_first
-    assert @assets.first, Fluxiom::Asset
+    assert_equal @assets.first.class, Fluxiom::Asset
   end
 
   def test_find
-    assert @assets.find(182535).id.to_i, 182535
+    assert_equal @assets.find(182535).id.to_i, 182535
   end
 
 end
